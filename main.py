9db3ea7233b5cb591e5b4a0fb86b416c6fe1b275
youssefkead <youssefkead@gmail.com>
2025-02-25 01:43:21 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Main program loop, responsibe for handling commands and processing module requests.
+import time
+
+startTime = time.time()
+time.sleep(3)
+
+def main():
+    time.sleep(3)
+
+endTime = time.time()
+duration = format(endTime - startTime,".0f")
+print(duration)
